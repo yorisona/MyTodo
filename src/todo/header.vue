04f0ac7d37ca1e4d78c4e10ec52eddbfbf678de8
linzhiqiang554940144 <45392200+linzhiqiang554940144@users.noreply.github.com>
2019-03-02 17:30:35 +0800

    MyTodo源码

---
+++ src/todo/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="main-header">
+        <h1>LLTodo</h1>
+    </header>
+</template>
+
+<style lang="stylus" scoped>
+    .main-header
+        text-align center
+        h1
+            font-size 100px
+            color rgba(248,147,29,0.4)
+            font-weight 400
+            margin 20px
+</style>
